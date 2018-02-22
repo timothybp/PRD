4d3df736f366c23908085606c1e3d5d3661373c5
Clotaire <clotaire.bi@gmmail.com>
2018-02-22 13:22:51 +0100

    加了运行时间

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from controllers.FileController import FileController
 from controllers.AlgorithmController import AlgorithmController
 from models.ant.AntModel import AntModel
-
+import time
 # C'est l'entrée du programme
 if __name__ == '__main__':
-
-    nbAnts = 1  # Le nombre de fourmis est 1
+    startTime = time.time()
+    nbAnts = 10  # Le nombre de fourmis est 1
     i=0
+    antList = []
     while i < nbAnts:
         ant = AntModel()
-        antList = []
         antList.append(ant)
         i += 1
 
-    nbIteration = 5 # Le nombre de l'itération est 5
+    nbIteration = 10 # Le nombre de l'itération est 5
     fc = FileController()
     fc.readFile()
 
@@ -22,4 +22,6 @@ if __name__ == '__main__':
     bestSolution = ac.allocateBuilding(nbIteration, antList, rayon)
 
     fc.writeFile(bestSolution)
+    endTime = time.time()
+    print(endTime - startTime)
 
